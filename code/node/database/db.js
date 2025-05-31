a1c626f2b12345f483f888af51ecaf208e987cb8
suheyon-son <ruminous0914@gmail.com>
2025-06-01 04:00:25 +0900

    db 환경변수 수정

--- code/node/database/db.js
+++ code/node/database/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
   host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
+  user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DATABASE
 });
