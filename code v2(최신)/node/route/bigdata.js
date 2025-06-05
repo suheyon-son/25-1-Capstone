e282abd23af11535563d4e7ee7dae0b638cf5d3a
chitenshi <119392889+chitenshi@users.noreply.github.com>
2025-06-05 11:31:10 +0900

    빅데이터: 계절별 통계를 season_name으로 화면에 표기할수 있는 컬럼 추가

--- code v2(최신)/node/route/bigdata.js
+++ code v2(최신)/node/route/bigdata.js
@@ -25,14 +25,21 @@ router.get('/api/pothole-monthly-count', (req, res) => {
 //!! 임시 조치 : 분기별로 처리
 router.get('/api/pothole-season-count', (req, res) => {
     const sql = `
-        SELECT
+        SELECT concat(year_date, '-', CASE season_rawdate
+                WHEN 1 THEN '봄'
+                WHEN 2 THEN '여름'
+                WHEN 3 THEN '가을'
+                WHEN 4 THEN '겨울'
+                ELSE '알 수 없음'
+            END) AS season_name, pothole_count
+        FROM (SELECT
             YEAR(pothole_date) AS year_date,
             QUARTER(pothole_date) AS season_rawdate,
             COUNT(pothole_id) AS pothole_count
         FROM pothole
         GROUP BY year_date, season_rawdate
         ORDER BY year_date DESC, season_rawdate DESC
-        LIMIT 8;
+        LIMIT 8) AS season_result;
     `;
     const values = [];
 
